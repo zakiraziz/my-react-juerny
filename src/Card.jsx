ce97523789610cf1f6ad958812a809f477670207
Zakir Aziz <zakir@example.com>
2025-03-14 20:10:41 +0500

    my new learning langueage

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    return(
+        <div className="card">
+         <img classname="card-image" src="https://files.oaiusercontent.com/file-84tao3x8Ar3JxtavrF5mgB?se=2025-03-13T13%3A47%3A08Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D8202d1b0-2f6a-464e-9b8a-ca8bf0e11381.webp&sig=i6nDBXmfWIxdqPURx2SQMLQl2K9zU4fRVgnkmGDt9Lc%3D" height="200px" />
+        <h2 classname="card-tittle">Eternal Solace</h2>
+        <p classname="card-text">Zakir Aziz is a young programmer passionate about coding and learning</p>
+        </div>
+    );
+}
+
+
+export default Card
