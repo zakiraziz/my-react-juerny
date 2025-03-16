de799acd9d50628b96ab78572aa02e9c322c3916
Zakir Aziz <zakir@example.com>
2025-03-16 19:40:44 +0500

    learning react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import Card from './Card.jsx'
+import Student from './Student.jsx'
 
-
-function App() {
-  return (
-    <>
-   <Card />
-   <Card />
-   <Card />
-   <Card />
-   </>
-  );
+function App(){
+  return(
+<>
+    <Student name="Spongebob" age={30} isStudent={true}/>
+    <Student name="Patrick" age={42} isStudent={false}/>
+    <Student name="Squidward" age={50} isStudent={false}/>
+    <Student name="Sandy" age={27} isStudent={true}/>
+</>
+  )
 }
-
 
 export default App
