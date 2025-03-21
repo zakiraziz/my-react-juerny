56aab1ee8e8845e9f582b271efa9223d801bec21
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-03-21 20:12:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import Student from './Student.jsx'
+import Button from  "./Button.jsx"
 
-function App(){
-  return(
-<>
-    <Student name="Spongebob" age={30} isStudent={true}/>
-    <Student name="Patrick" age={42} isStudent={false}/>
-    <Student name="Squidward" age={50} isStudent={false}/>
-    <Student name="Sandy" age={27} isStudent={true}/>
-</>
-  )
+
+function App() {
+
+  return(<button />);
+  
 }
+
 
 export default App
