69f9aa8e05cb3ab71b23772da38b2e5e976706d2
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-04-10 20:05:37 +0500

    Update ToDolist.jsx

--- src/ToDolist.jsx
+++ src/ToDolist.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react"
 
 function ToDolist(){
 
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTasks] = useState("");
+    const [tasks, setTasks] = useState(["Eat Breakfast", "Take a shower", "walkthe dog"]);
+    const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
-
+        setNewTask(event.target.value)
     }
 
     function AddTask(){
@@ -34,7 +34,35 @@ function ToDolist(){
                 placeholder="Enter a task..."
                 value={newTask}
                 onChange={handleInputChange} />
+                <button
+                    className="add-button"
+                    onClick={AddTask}>
+                    Add
+                    </button>
             </div>
+
+            <ol>
+                {tasks.map((tasks, index) =>
+                <li key={index}>
+                    <span className="text">{tasks}</span>
+                    <button
+                    className="delete-butto"
+                    onClick={() => deleteTask(index)}>
+                        Delete
+                    </button>
+                    <button 
+                    className="move-button"
+                    onClick={() => moveTaskUp(index)}>
+                        :point_up:
+                    </button>
+                    <button 
+                    className="move-button"
+                    onClick={() => moveTaskUp(index)}>
+                        down
+                    </button>
+                </li>
+            )}
+            </ol>
         </div>
     )    
 }
