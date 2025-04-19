0affbca3e35f2064c7587838e4283b98b6d0a4b7
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-04-19 21:32:11 +0500

    Update CompnonentA.jsx

--- src/CompnonentA.jsx
+++ src/CompnonentA.jsx
@@ -1,5 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, createContext} from "react";
 import ComponentB from "./CompnonentB.react";
+
+export const UserContext = createContext();
 
 function CompnonentA(){
 
@@ -9,7 +11,9 @@ function CompnonentA(){
         <div className="box">
             <h1>ComponentA</h1>
             <h2>{`Hello ${user}`}</h2>
+            <UserContext.provider value={user}/>
             <ComponentB user={user}/>
+            <UserContext.provider/>
         </div>
     )
 }
