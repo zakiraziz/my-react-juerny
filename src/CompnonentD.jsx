72099a88a390f9668d7a7d8cda5f19ec52348ad4
zakiraziz <118102745+zakiraziz@users.noreply.github.com>
2025-04-19 21:32:49 +0500

    Update CompnonentD.jsx

--- src/CompnonentD.jsx
+++ src/CompnonentD.jsx
@@ -1,9 +1,11 @@
-
+import React, {useContext} from "react";
+import { UserContext } from "./CompnonentA.jsx";
 function CompnonentD(){
 
     return(
         <div className="box">
             <h1>ComponentD</h1>
+            <h2>{`bye ${user}`}</h2>
         </div>
     )
 }
